c6120dab1a2867ae76125344239d431b231e9f11
Jake Van Noort <jakevn@gmail.com>
2019-01-15 18:47:26 -0500

    use switch/case instead of jump table for ops

--- vm.go
+++ vm.go
@@ -4,107 +4,81 @@ import "log"
 
 func Run(p *Bytecode) {
 	for p.pos < len(p.OpAddrs) {
-		funcAddrs[p.OpAddrs[p.pos]](p)
-	}
-}
-
-var funcAddrs = []func(*Bytecode){
-	func(p *Bytecode) { // 0: iopIntCopy (int int)
-		p.Ints[p.OpAddrs[p.pos+2]] = p.Ints[p.OpAddrs[p.pos+1]]
-		p.pos += 3
-	},
-	func(p *Bytecode) { // 1: iopStrCopy (str str)
-		p.Strs[p.OpAddrs[p.pos+2]] = p.Strs[p.OpAddrs[p.pos+1]]
-		p.pos += 3
-	},
-	func(p *Bytecode) { // 2: iopBoolCopy (bool bool)
-		p.Bools[p.OpAddrs[p.pos+2]] = p.Bools[p.OpAddrs[p.pos+1]]
-		p.pos += 3
-	},
-	func(p *Bytecode) { // 3: != (int int) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] != p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 4: != (str str) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Strs[p.OpAddrs[p.pos+1]] != p.Strs[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 5: % (int int) -> int
-		p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] % p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 6: && (bool bool) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Bools[p.OpAddrs[p.pos+1]] && p.Bools[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 7: * (int int) -> int
-		p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] * p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 8: + (int int) -> int
-		p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] + p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 9: + (str str) -> str
-		p.Strs[p.OpAddrs[p.pos+3]] = p.Strs[p.OpAddrs[p.pos+1]] + p.Strs[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 10: - (int int) -> int
-		p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] - p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 11: / (int int) -> int
-		p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] / p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 12: < (int int) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] < p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 13: <= (int int) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] <= p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 14: == (int int) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] == p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 15: == (str str) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Strs[p.OpAddrs[p.pos+1]] == p.Strs[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 16: > (int int) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] > p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 17: >= (int int) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] >= p.Ints[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
-	func(p *Bytecode) { // 18: goto (addr)
-		p.pos = p.Ints[p.OpAddrs[p.pos+1]]
-	},
-	func(p *Bytecode) { // 19: if (bool)
-		if p.Bools[p.OpAddrs[p.pos+1]] {
+		switch p.OpAddrs[p.pos] {
+		case 0: // 0: iopIntCopy (int int)
+			p.Ints[p.OpAddrs[p.pos+2]] = p.Ints[p.OpAddrs[p.pos+1]]
 			p.pos += 3
-		} else {
-			p.pos = p.OpAddrs[p.pos+2]
+		case 1: // 1: iopStrCopy (str str)
+			p.Strs[p.OpAddrs[p.pos+2]] = p.Strs[p.OpAddrs[p.pos+1]]
+			p.pos += 3
+		case 2: // 2: iopBoolCopy (bool bool)
+			p.Bools[p.OpAddrs[p.pos+2]] = p.Bools[p.OpAddrs[p.pos+1]]
+			p.pos += 3
+		case 3: // 3: != (int int) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] != p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 4: // 4: != (str str) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Strs[p.OpAddrs[p.pos+1]] != p.Strs[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 5: // 5: % (int int) -> int
+			p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] % p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 6: // 6: && (bool bool) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Bools[p.OpAddrs[p.pos+1]] && p.Bools[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 7: // 7: * (int int) -> int
+			p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] * p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 8: // 8: + (int int) -> int
+			p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] + p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 9: // 9: + (str str) -> str
+			p.Strs[p.OpAddrs[p.pos+3]] = p.Strs[p.OpAddrs[p.pos+1]] + p.Strs[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 10: // 10: - (int int) -> int
+			p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] - p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 11: // 11: / (int int) -> int
+			p.Ints[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] / p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 12: // 12: < (int int) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] < p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 13: // 13: <= (int int) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] <= p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 14: // 14: == (int int) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] == p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 15: // 15: == (str str) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Strs[p.OpAddrs[p.pos+1]] == p.Strs[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 16: // 16: > (int int) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] > p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 17: // 17: >= (int int) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Ints[p.OpAddrs[p.pos+1]] >= p.Ints[p.OpAddrs[p.pos+2]]
+			p.pos += 4
+		case 18: // 18: goto (addr)
+			p.pos = p.Ints[p.OpAddrs[p.pos+1]]
+		case 19: // 19: if (bool)
+			if p.Bools[p.OpAddrs[p.pos+1]] {
+				p.pos += 3
+			} else {
+				p.pos = p.OpAddrs[p.pos+2]
+			}
+		case 20: // 20: print (str)
+			log.Println(p.Strs[p.OpAddrs[p.pos+1]])
+			p.pos += 2
+		case 21: // 21: print (int)
+			log.Println(p.Ints[p.OpAddrs[p.pos+1]])
+			p.pos += 2
+		case 22: // 22: print (bool)
+			log.Println(p.Bools[p.OpAddrs[p.pos+1]])
+			p.pos += 2
+		case 23: // 23: || (bool bool) -> bool
+			p.Bools[p.OpAddrs[p.pos+3]] = p.Bools[p.OpAddrs[p.pos+1]] || p.Bools[p.OpAddrs[p.pos+2]]
+			p.pos += 4
 		}
-	},
-	func(p *Bytecode) { // 20: print (str)
-		log.Println(p.Strs[p.OpAddrs[p.pos+1]])
-		p.pos += 2
-	},
-	func(p *Bytecode) { // 21: print (int)
-		log.Println(p.Ints[p.OpAddrs[p.pos+1]])
-		p.pos += 2
-	},
-	func(p *Bytecode) { // 22: print (bool)
-		log.Println(p.Bools[p.OpAddrs[p.pos+1]])
-		p.pos += 2
-	},
-	func(p *Bytecode) { // 23: || (bool bool) -> bool
-		p.Bools[p.OpAddrs[p.pos+3]] = p.Bools[p.OpAddrs[p.pos+1]] || p.Bools[p.OpAddrs[p.pos+2]]
-		p.pos += 4
-	},
+	}
 }
